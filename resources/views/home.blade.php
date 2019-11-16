22885819c4ee0018dba0c48abcbda909c97eb2d1
Vikas Kushwha <vikas.kushwha@synapse.com>
2019-11-16 15:19:05 +0530

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Id</th>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td><a href="">Delete</a></td>
+    </tr>
+  </tbody>
+</table>
+@endsection
