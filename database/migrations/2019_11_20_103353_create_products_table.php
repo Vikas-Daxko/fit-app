fe08554686a4c1e96e4001d6bf5ab54dd907fdc4
Vikas Kushwha <vikas.kushwha@synapse.com>
2019-11-21 15:00:21 +0530

    product migration fixes

--- database/migrations/2019_11_20_103353_create_products_table.php
+++ database/migrations/2019_11_20_103353_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
 			$table->string('name', 200);
-			$table->integer('price',11);
+			$table->integer('price');
             $table->text('decscription');
             $table->string('image_url');
             $table->timestamps();
